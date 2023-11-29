f2a090c093c0c484f620a924e5eb7f0ca43280b6
LevWorking <63475577+LevWorking@users.noreply.github.com>
2023-11-29 12:37:46 -0500

    Basic grid generation works.

--- CGT215_GameOfLife.cpp
+++ CGT215_GameOfLife.cpp
@@ -4,18 +4,25 @@
 #include <iostream>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include <random>
 
 using namespace std;
 using namespace sf;
 
 //Set the dimensions of the grid.
-const int gridX = 10;
-const int gridY = 10;
+const int gridX = 25;
+const int gridY = 20;
+const int buffer = 100;
+
+
+RenderWindow window(VideoMode(800, 600), "Game of Life");
+int cellSize = (window.getSize().x - buffer) / max(gridX, gridY);
+int border = 3;
+RectangleShape cell[gridX][gridY];
 
 int currentStep[gridX][gridY];
 int nextStep[gridX][gridY];
-
 
 //Initialize the arrays. 
 void initialize()
@@ -26,6 +33,10 @@ void initialize()
         {
             currentStep[x][y] = 0;
             nextStep[x][y] = 0;
+
+            cell[x][y].setSize(Vector2f(cellSize - border, cellSize - border));
+            cell[x][y].setFillColor(Color(255, 255, 255));
+            cell[x][y].setPosition(Vector2f(cellSize * x, cellSize * y));
         }
     }
 }
@@ -53,16 +64,82 @@ void randomizeGrid()
 }
 
 //Consulting the currentArray, construct the next step in the nextStep array, then reassign that into the current state.
-void simulateStep() {}
+void simulateStep() 
+{
+    int alive = 0;
 
-void drawGrid() {} 
+    for (int x = 0; x < gridX; x++)
+    {
+        for (int y = 0; y < gridY; y++)
+        {
+            if (x > 0) {
+                if (y > 0) 
+                {
+                    if (currentStep[x - 1][y - 1] == 1) { alive++; }
+                }
+
+                if (currentStep[x - 1][y] == 1) { alive++; }
+
+                if (y < gridY) 
+                {
+                    if (currentStep[x - 1][y+1] == 1) { alive++; }
+                }
+            }
+
+            if (y > 0) 
+            {
+                if(x < gridX)
+                { 
+                    if (currentStep[x + 1][y - 1] == 1) { alive++; } 
+                }
+
+                if (currentStep[x][y - 1] == 1) { alive++; }
+                
+            }
+
+            if (x < gridX) 
+            {
+                if (currentStep[x + 1][y] == 1) { alive++; }
+                
+                if(y < gridY)
+                {
+                    if (currentStep[x + 1][y + 1] == 1) { alive++; }
+                }
+            }
+
+            if (y < gridY) 
+            {
+                if (currentStep[x][y + 1] == 1) { alive++; }
+            }
+
+        }
+    }
+}
+
+void drawGrid() 
+{
+    window.clear();
+
+    for (int x = 0; x < gridX; x++)
+    {
+        for (int y = 0; y < gridY; y++)
+        {
+            window.draw(cell[x][y]);
+        }
+    }
+
+    
+} 
 
 int main()
 {
-    
-    RenderWindow window(VideoMode(800, 600), "Game of Life");
+   
     initialize();
     randomizeGrid();
 
-    //do { simulateStep(); drawGrid(); } while (true);
+    do { 
+        simulateStep(); 
+        drawGrid();
+        window.display();
+    } while (true);
 }
